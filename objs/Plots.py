f721909a31c8137c3a76a6a8cf44792bc54777f7
lucasosiewicz <osiewicz87@gmail.com>
2023-11-29 19:32:24 +0100

    Fixed toolbar in Plot class

--- objs/Plots.py
+++ objs/Plots.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from tkinter import ttk
 from matplotlib.figure import Figure
 from scipy.signal import savgol_filter
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 # Classes
 from objs.Dataset import *
@@ -14,6 +14,10 @@ class Plots(ttk.Frame):
     def __init__(self, parent, dataset_didu, dataset_current, number_of_points):
         super().__init__(parent)
 
+        # Set grid
+        self.columnconfigure((0,1,2,3), weight=1, uniform='a')
+        self.rowconfigure(0, weight=1, uniform='a')
+
         # Class variables
         self.dataset_current = dataset_current.iloc[:, -number_of_points-2:-2]
         self.dataset_didu = dataset_didu.iloc[:, -number_of_points-2:-2]
@@ -24,21 +28,23 @@ class Plots(ttk.Frame):
         # Labels variables
         self.checkbox_vars = [tk.BooleanVar(value=True) for _ in range(self.number_of_points)]
         self.x_range = [tk.StringVar(value=f'{min(self.x)}'), tk.StringVar(value=f'{max(self.x)}')]
+        self.savgol_var = tk.IntVar(value=3)
 
         # Widgets
         self.plot_frame = self.create_plot_frame()
         self.navigation_toolbar = self.create_navitagion_toolbar()
 
-
-
         # Layout
-        self.plot_frame.place(relx=0, rely=0, anchor='nw', relwidth=0.66, relheight=1)
-        self.navigation_toolbar.place(relx=0.8, rely=0, anchor='nw', relwidth=0.9, relheight=0.5)
+        self.plot_frame.grid(row=0, column=0, columnspan=3, sticky='news')
+        self.navigation_toolbar.grid(row=0, column=3, sticky='news')
         self.pack(expand=True, fill='both')
 
     def create_plot_frame(self):
         # Funkcja tworzy obiekt frame z wykresami
         frame = ttk.Frame(self)
+
+        frame.rowconfigure(0, weight=1, uniform='a')
+        frame.columnconfigure(0, weight=1, uniform='a')
 
         fig = Figure()
         current_plot = fig.add_subplot(2, 1, 1)
@@ -51,7 +57,7 @@ class Plots(ttk.Frame):
 
         canvas = FigureCanvasTkAgg(fig, frame)
         canvas.draw()
-        canvas.get_tk_widget().pack(expand=True, fill='both')
+        canvas.get_tk_widget().grid(row=0, column=0, sticky='news')
 
         return frame
 
@@ -59,16 +65,17 @@ class Plots(ttk.Frame):
         plot.clear()
 
         y_values = y_data.values.T
-
+        xlim_min, xlim_max = self.get_entry_values()
 
         for y, column, idx in zip(y_values, self.columns, range(len(self.checkbox_vars))):
             if self.checkbox_vars[idx].get():
-                y = savgol_filter(y, 35, 2)
+                y = savgol_filter(y, self.savgol_var.get(), 2)
                 plot.plot(self.x, y, label=column, linewidth=0.7)
 
         plot.set_title(title)
         plot.grid(True)
         plot.set_ylabel(yname)
+        plot.set_xlim(xlim_min, xlim_max)
         if first_plot:
             plot.set_xlabel('x [V]')
         else:
@@ -77,10 +84,14 @@ class Plots(ttk.Frame):
     def create_navitagion_toolbar(self):
         frame = ttk.Frame(self)
 
-        self.create_checkboxes(frame).pack(fill='both')
-        self.create_x_range_entries(frame).pack(fill='both')
-        ttk.Button(frame, text='Refresh', command=self.refresh_plot).pack(fill='both')
-        ttk.Button(frame, text='Add curves', command=self.create_adding_curves_window).pack(fill='both')
+        frame.rowconfigure((0,1,2,3,4,5), weight=1, uniform='a')
+        frame.columnconfigure(0, weight=1, uniform='a')
+
+        self.create_checkboxes(frame).grid(row=0, column=0, rowspan=2, sticky='news', padx=10, pady=10)
+        self.create_x_range_entries(frame).grid(row=2, column=0, sticky='news', padx=10, pady=10)
+        self.create_savgol_slider(frame).grid(row=3, column=0, sticky='news')
+        ttk.Button(frame, text='Add curves', command=self.create_adding_curves_window).grid(row=4, column=0)
+        ttk.Button(frame, text='Refresh', command=self.refresh_plot).grid(row=5, column=0)
 
         return frame
 
@@ -92,31 +103,52 @@ class Plots(ttk.Frame):
                             text=f'{column}',
                             variable=self.checkbox_vars[idx],
                             onvalue=True,
-                            offvalue=False).pack(expand=True, fill='both')
+                            offvalue=False).pack(expand=True)
         return frame
 
     def create_x_range_entries(self, parent):
         frame = ttk.Frame(parent)
 
-        ttk.Entry(frame, textvariable=self.x_range[0]).pack(fill='both')
-        ttk.Entry(frame, textvariable=self.x_range[1]).pack(fill='both')
+        frame.rowconfigure((0,1), weight=1, uniform='a')
+        frame.columnconfigure((0,1), weight=1, uniform='a')
+
+        ttk.Label(frame, text='Set min X on axis:').grid(row=0, column=0)
+        ttk.Label(frame, text='Set max X on axis:').grid(row=1, column=0)
+        ttk.Entry(frame, textvariable=self.x_range[0]).grid(row=0, column=1)
+        ttk.Entry(frame, textvariable=self.x_range[1]).grid(row=1, column=1)
+
+        return frame
+
+    def create_savgol_slider(self, parent):
+        frame = ttk.Frame(parent)
+
+        frame.columnconfigure(0, weight=1, uniform='a')
+        frame.rowconfigure((0,1,2), weight=1, uniform='a')
+
+        ttk.Label(frame, text='Set level of smoothing plots:').grid(row=0, column=0)
+        ttk.Scale(frame, variable=self.savgol_var,
+                  from_=3,
+                  to=len(self.x) // 5,
+                  command=lambda event: self.savgol_var.set(round(int(float(event))))).grid(row=1, column=0)
+        ttk.Label(frame, textvariable=self.savgol_var).grid(row=2, column=0)
 
         return frame
 
     def create_adding_curves_window(self):
         AddingCurvesWindow(self.dataset_didu, self.x)
 
-
     # Event functions
     def refresh_plot(self):
         self.plot_frame.pack_forget()
         self.plot_frame = self.create_plot_frame()
-        self.plot_frame.place(relx=0, rely=0, anchor='nw', relwidth=0.66, relheight=1)
+        self.plot_frame.grid(row=0, column=0, columnspan=3, sticky='news')
 
     def get_entry_values(self):
         try:
             return [float(self.x_range[0].get()), float(self.x_range[1].get())]
-        except TypeError:
+        except ValueError:
+            self.x_range[0].set(min(self.x))
+            self.x_range[1].set(max(self.x))
             print('Not a digit in entry')
             return [min(self.x), max(self.x)]
 
