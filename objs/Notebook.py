aeb687884e09f18424ca85d46f8792b97671bff0
lucasosiewicz <osiewicz87@gmail.com>
2023-12-07 10:28:26 +0100

    Added spectra topography

--- objs/Notebook.py
+++ objs/Notebook.py
@@ -2,8 +2,10 @@ import tkinter as tk
 from tkinter import ttk
 
 # Classes
-from objs.Dataset import *
 from objs.Plots import *
+from objs.Dataset import *
+from objs.Topography import *
+
 
 class Notebook(ttk.Notebook):
     def __init__(self, parent, file, curves_per_point, number_of_points, forward_or_backward):
@@ -11,7 +13,9 @@ class Notebook(ttk.Notebook):
         # Tabs
         self.dataset = Dataset(self, file, curves_per_point, number_of_points, forward_or_backward)
         self.plots = Plots(self, self.dataset.dataset_dIdU, self.dataset.dataset_current, number_of_points)
+        self.topography = Topography(self, file)
 
         # Adding Tabs to the Notebook
         self.add(self.dataset, text='Dataset')
         self.add(self.plots, text='Plots')
+        self.add(self.topography, text='Topography')
---
+++ objs/Topography.py
@@ -0,0 +1,55 @@
+import tkinter as tk
+from tkinter import ttk
+import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
+
+class Topography(ttk.Frame):
+    def __init__(self, parent, file):
+        super().__init__(parent)
+
+        self.tf = file['Topography_Forward']
+        self.tb = file['Topography_Backward']
+        self.didu_f = file['dI/dU__Forward']
+        self.didu_b = file['dI/dU__Backward']
+
+        self.create_topography().pack(expand=True, fill='both')
+
+        self.pack(expand=True, fill='both')
+
+    def create_topography(self):
+        frame = ttk.Frame(self)
+
+        fig = Figure()
+        for i, dataset, name in zip([1,3], [self.tf, self.tb], ['Forward', 'Backward']):
+            ax = fig.add_subplot(2, 2, i)
+            x = dataset[f'Topography_{name}_x']
+            y = dataset[f'Topography_{name}_y']
+            z = dataset.values
+            im = ax.pcolormesh(x, y, z)
+            ax.set_title(name)
+            divider = make_axes_locatable(ax)
+            cax = divider.append_axes("right", size="5%", pad=0.1)
+            colorbar = plt.colorbar(im, cax=cax)
+
+        for i, dataset, name in zip([2,4], [self.didu_f, self.didu_b], ['dI/dU__Forward', 'dI/dU__Backward']):
+            ax = fig.add_subplot(2, 2, i)
+            x = dataset[f'{name}_x']
+            y = dataset[f'{name}_y']
+            z = dataset.values
+            im = ax.pcolormesh(x, y, z)
+            ax.set_title(name)
+            divider = make_axes_locatable(ax)
+            cax = divider.append_axes("right", size="5%", pad=0.1)
+            colorbar = plt.colorbar(im, cax=cax)
+
+        canvas = FigureCanvasTkAgg(fig, frame)
+        canvas.draw()
+        canvas.get_tk_widget().pack(expand=True, fill='both')
+
+        return frame
+
+
+
